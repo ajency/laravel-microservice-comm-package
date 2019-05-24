30e0fa87979ddb9e2fd5ef24e0b7df7fca922b74
VirtualBox <shashank@ajency.in>
2019-05-24 11:18:29 +0530

    move to register

--- src/ServiceCommServiceProvider.php
+++ src/ServiceCommServiceProvider.php
@@ -16,23 +16,13 @@ class ServiceCommServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton(SnsClient::class, function ($app, $config){
-            return new SnsClient(config('service_comm.sns.client'));
-        });
-
-        $this->app->singleton(SNS::class, function ($app, $config) {
-            return new SNS($app[SnsClient::class]);
-        });
+       
 
         $this->publishes([
             __DIR__ . '/config/service_comm.php' => config_path('service_comm.php'),
         ], 'config');
 
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Create::class,
-            ]);
-        }
+        
     }
 
     /**
@@ -42,6 +32,18 @@ class ServiceCommServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(SnsClient::class, function ($app, $config){
+            return new SnsClient(config('service_comm.sns.client'));
+        });
+
+        $this->app->singleton(SNS::class, function ($app, $config) {
+            return new SNS($app[SnsClient::class]);
+        });
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Create::class,
+            ]);
+        }
     }
 }
